add94f2ff98ef79303c3f1c474bbcbfc2c32a72c
FernandoSL <nando.leandro@hotmail.com>
2019-10-04 16:17:35 -0300

    Implement the base object
    Create a class GameObject.cs the base class for all objects in the game.

---
+++ TopDownShooter/GameObjects/GameObject.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TopDownShooter.GameObjects
+{
+    public class GameObject
+    {
+        public Texture2D Texture { get; private set; }
+
+        public Vector2 Position { get; private set; }
+
+        public GameObject(Texture2D texture, Vector2 position)
+        {
+            this.Texture = texture;
+            this.Position = position;
+        }
+
+        public virtual void MoveOnX(int pixels)
+        {
+            Vector2 position = this.Position;
+            position.X = this.Position.X + pixels;
+
+            this.Position = position;
+        }
+
+        public virtual void MoveOnY(int pixel)
+        {
+            Vector2 position = this.Position;
+            position.Y = this.Position.Y + pixel;
+
+            this.Position = position;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(0, 0, this.Texture.Width, this.Texture.Height);
+            Vector2 origin = new Vector2(this.Texture.Width / 2, this.Texture.Height / 2);
+
+            spriteBatch.Draw(this.Texture, this.Position, sourceRectangle, Color.White, 0, origin, 1.0f, SpriteEffects.None, 1);
+        }
+    }
+}
